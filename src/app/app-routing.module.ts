a5ad76976d31029c8ff1ed646568a2333a060f37
pratik29046 <79098295+pratik29046@users.noreply.github.com>
2021-09-05 22:47:38 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './my-componet/about/about.component';
+import { ContactComponent } from './my-componet/contact/contact.component';
+import { HomeComponent } from './my-componet/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home" , component:HomeComponent},
+  {path:"about" , component:AboutComponent},
+  {path:'contact' , component:ContactComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './my-componet/home/home.component';
+import { AboutComponent } from './my-componet/about/about.component';
+import { ContactComponent } from './my-componet/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
